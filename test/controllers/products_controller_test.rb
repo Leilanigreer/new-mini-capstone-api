31e4dd05f936db5c1d9811cabb2b20f378f1fc35
Leilanigreer <leilani@lpcgolf.com>
2024-09-10 11:37:55 -0700

    added tests for index and create

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "index" do
+    get "/products.json"
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal Product.count, data.length
+  end
+
+  test "create" do
+    assert_difference "Product.count", 1 do
+      post "/products.json", params: { name: "lake", price: 2, description: "Yum Yum", image_url: "google.com" }
+      assert_response 200
+    end
+  end
 end
