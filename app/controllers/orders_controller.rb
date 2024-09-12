d8efaa023c5a4c92ca307c1e1c6cacd458d09a65
Leilanigreer <leilani@lpcgolf.com>
2024-09-11 19:33:28 -0700

    added controller and jbuilder for Order. Also made hard coded Create action along with show and index

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,27 @@
+class OrdersController < ApplicationController
+  def index
+    @orders = Order.all
+    render :index
+  end
+
+  def create
+    @order = Order.new(
+      user_id: params[:user_id],
+      product_id: params[:product_id],
+      quantity: params[:quantity],
+      subtotal: params[:subtotal],
+      tax: params[:tax],
+      total: params[:total],
+    )
+    if @order.save
+      render :show
+    else
+      render json: { errors: @order.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @order = Order.find_by(id: params[:id])
+    render :show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -29,6 +29,10 @@ Rails.application.routes.draw do
 
   post "/sessions" => "sessions#create"
 
+  get "/orders" => "orders#index"
+  post "/orders" => "orders#create"
+  get "/orders/:id" => "orders#show"
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
