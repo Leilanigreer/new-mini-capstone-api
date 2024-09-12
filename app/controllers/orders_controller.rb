5f8a5fde50b7ccf0c2191c7ec2b6d0de8c4b14b0
Leilanigreer <leilani@lpcgolf.com>
2024-09-12 15:22:51 -0700

    added real current user to order creation

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,18 +5,25 @@ class OrdersController < ApplicationController
   end
 
   def create
-    @order = Order.new(
-      user_id: params[:user_id],
-      product_id: params[:product_id],
-      quantity: params[:quantity],
-      subtotal: params[:subtotal],
-      tax: params[:tax],
-      total: params[:total],
-    )
-    if @order.save
-      render :show
+    pp "This IS THE CURRENT USER"
+    pp current_user
+    pp "THAT WAS THE CURRENT"
+    if current_user != nil
+      @order = Order.new(
+        user_id: current_user.id,
+        product_id: params[:product_id],
+        quantity: params[:quantity],
+        subtotal: params[:subtotal],
+        tax: params[:tax],
+        total: params[:total],
+      )
+      if @order.save
+        render :show
+      else
+        render json: { errors: @order.errors.full_messages }, status: :unprocessable_entity
+      end
     else
-      render json: { errors: @order.errors.full_messages }, status: :unprocessable_entity
+      render json: { errors: "You must be logged in to place an order" }
     end
   end
 
