dc52dd89fd3bab40519630ae96c74b4711d1f5c9
Leilanigreer <leilani@lpcgolf.com>
2024-12-03 07:49:38 -0800

    cleaned up test jwt

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,15 +3,12 @@ require_relative "../config/environment"
 require "rails/test_help"
 require "database_cleaner/active_record"
 
-# Configure Database Cleaner
 DatabaseCleaner.strategy = :transaction
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
   setup do
-    # This ensures we start with a clean database for each test
     DatabaseCleaner.start
   end
 
